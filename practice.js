4618581c038deb53e26cdece18902ebcc512d32a
dennis juanito <dennisjuanito@denniss-MacBook-Pro-2.local>
2018-06-26 14:28:10 -0600

    final commit

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, callback) {
+  callback(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, callback) {
+  return callback(arr[arr.length - 1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, callback) {
+  return callback(num1 * num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,13 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, callback) {
+  if (arr.includes(name)) {
+    callback(true);
+  } else {
+    callback(false);
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +122,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, callback) {
+  var uniqArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (uniqArr.indexOf(arr[i]) === -1) {
+      uniqArr.push(arr[i]);
+    }
+  }
+  return callback(uniqArr);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +148,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i); 
+  };
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +170,14 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(arr, id, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      callback(arr[i]);
+    }
+
+  };
+}
 
 // Do not edit the code below.
 var users = [
@@ -167,3 +205,6 @@ getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
 // Do not edit the code above.
+
+
+
